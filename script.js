0c4c682b3c1acc75e6354e7e41c3beeb49f991a9
ssocolow <47473649+ssocolow@users.noreply.github.com>
2020-04-09 21:50:31 -0400

    Update script.js

--- script.js
+++ script.js
@@ -5,16 +5,17 @@ function inc()
   x++;
   console.log(x);
 }
-
+let narutopic = "https://vignette.wikia.nocookie.net/naruto/images/d/dd/Naruto_Uzumaki%21%21.png/revision/latest?cb=20161013233552";
+let blackholepic = "https://static01.nyt.com/images/2019/04/10/science/event-horizon-black-hole-images-1554901170942/event-horizon-black-hole-images-1554901170942-articleLarge-v2.jpg"
 function changePic()
 {
   s = document.getElementById('pic').src;
-  if (s == "file:///C:/Users/Simon%20Socolow/Desktop/website/blackhole.jpg")
+  if (s == blackholepic)
   {
-    document.getElementById('pic').src = "file:///C:/Users/Simon%20Socolow/Desktop/website/naruto.jpg";
+    document.getElementById('pic').src = narutopic;
   }
   else
   {
-    document.getElementById('pic').src = "file:///C:/Users/Simon%20Socolow/Desktop/website/blackhole.jpg";
+    document.getElementById('pic').src = blackholepic;
   }
 }
