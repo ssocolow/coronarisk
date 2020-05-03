2ebe0b1b4a995b4e4de3b3f8a1d3312512ef3e3f
ssocolow <ssocolow@github.com>
2020-05-02 20:09:25 -0400

    added static data and cleaned up old stuff

---
+++ static_data.js
@@ -0,0 +1,24 @@
+let static_data = {
+
+//format is total population then percent female
+"Androscoggin" : [107444,0.512],
+"Aroostook" : [68269,0.512],
+"Cumberland" : [290944,0.513],
+"Franklin" : [30019,0.503],
+"Hancock" : [54541,0.508],
+"Kennebec" : [121545,0.513],
+"Knox" : [39823,0.507],
+"Lincoln" : [34067,0.492],
+"Oxford" : [57325,0.51],
+"Penobscot" : [151748,0.51],
+"Piscataquis" : [16887,0.511],
+"Sagadahoc" : [35277,0.506],
+"Somerset" : [50710,0.51],
+"Waldo" : [39418,0.509],
+"Washington" : [31694,0.509],
+"York" : [203102,0.512]
+
+
+
+
+}
