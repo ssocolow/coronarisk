73988417409a8d4d176699322618200f9ca2c59e
ssocolow <ssocolow@github.com>
2020-05-19 11:00:30 -0400

    age selection now works

--- coronascript.js
+++ coronascript.js
@@ -2,26 +2,31 @@ function calculate()
 {
 	//convert the strings of numbers to integer numbers
 	convertStringsToNums();
+	
+	//get an array with probabilities for each gender in the county pop
+	let gender_probs = getGenderProbs();
 
 	//get county, age, and gender data
 	let county_num = document.querySelector("#county").value;
 	let gender = document.querySelector("#gender").value;
 	let age = document.querySelector("#age").value;
 
-	//gender population and infected
-	let female_pop = astatic_data[county_num][1] * astatic_data[county_num][0];
-	let male_pop = astatic_data[county_num][0] - female_pop;	
-	let gender_pops = [male_pop, female_pop, astatic_data[county_num][0]];
-	let gender_infected = [updated_data[county_num][1] * 0.638, updated_data[county_num][1] * 0.362, updated_data[county_num][1]];
-	
-	//age population and infected
-	let age_pop = astatic_data[county_num][0] * county_age_data[age];
-	let age_infected = updated_data[county_num][1] * corona_age_data[age];
+	//have array of gender probabilities according to corona study
+	//goes male, female, total
+	gender_probs_i = [0.638, 0.362, 1];
 
-	//old and will be updated once I figure stuff out
-	let prob_i = gender_infected[gender] / gender_pops[gender]
+	//get total population of county
+	let tcpop = astatic_data[county_num][0];
 
-	//let prob_i = updated_data[county_num][1] / astatic_data[county_num][0]; 	
+	//get the number of infected that belong to the selected demographic
+	let dpop_i = updated_data[county_num][1] * gender_probs_i[gender] * corona_age_data[age];
+
+	//get the number of people in the county that belong to the selected demographic
+	let dpop = tcpop * gender_probs[county_num][gender] * county_age_data[age];
+
+	//the probability of being infected is the infected selected demographic pop over total demographic pop
+	let prob_i = dpop_i / dpop;
+
 	document.querySelector('#p_infected').innerHTML = prob_i*100 + "%";
 	
 	let prob_d = prob_i * (updated_data[county_num][2]/updated_data[county_num][1]);
@@ -33,28 +38,22 @@ function calculate()
 	{
 		document.querySelector('#p_dying').innerHTML = prob_d*100 + "%";
 	}
+}
 
 
-
-
-
-
-/**let age = document.querySelector("#age").value;
-    let health = document.querySelector("#health").value;
-
-    console.log(state,age,health);
-
-    if ((state === 'ME') && (age == 2) && (health == 1))
-    {
-        document.querySelector('#p_infected').innerHTML = '100%';
-        document.querySelector('#p_dying').innerHTML = '100%';
-    }
-    else
-    {
-        document.querySelector('#p_infected').innerHTML = 'NA';
-        document.querySelector('#p_dying').innerHTML = 'NA';
-    }
-**/
+//function that returns array of probabilities of gender for each county
+function getGenderProbs()
+{
+	let p_arr = [];
+	for(let i = 0; i < 16; i++)
+	{
+		p_arr.push([]);
+		p_arr[i][0] = 1 - astatic_data[i][1];
+		p_arr[i][1] = astatic_data[i][1];
+		p_arr[i][2] = 1;
+	}
+	
+	return p_arr;
 }
 
 function convertStringsToNums()
